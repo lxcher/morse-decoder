5cc9489ac8dd881a730b748bd8893cee9546de50
lxcher <aleksejcerenkevic@gmail.com>
2022-07-13 23:56:24 +0300

    fix: add code to index.js

--- src/index.js
+++ src/index.js
@@ -35,12 +35,32 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = []
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i + 10))
+    }
+    let morseArr = []
+    for (let el of arr) {
+        let str = ''
+        if (el === '**********') str = '**********'
+        for (let j = 0; j < el.length; j += 2) {
+            let substr = el[j] + el[j+1]
+            if (substr === '00') str = str + ''
+            if (substr === '10') str = str + '.'
+            if (substr === '11') str = str + '-'
+        }
+        morseArr.push(str)
+    }
+    let result = ''
+    for (let el of morseArr) {
+        result = result + MORSE_TABLE[el]
+    }
+    return result
 }
-
 module.exports = {
     decode
 }
